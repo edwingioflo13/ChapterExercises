6da61c58026c48b853a9d39b550f91c22c569228
Sergio Hidalgo-Espinoza <shhidalg@pichincha.com>
2021-11-19 10:10:34 -0500

    init game

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,9 +2,15 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
+    int[] scores = {0, 15, 30, 40};
+    int scorePlayer1 = 0;
+    int scorePlayer2 = 0;
 
+    boolean gameOn = true;
 
+    do {
 
-    }
+    } while (gameOn);
+  }
 }
